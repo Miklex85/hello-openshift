8cb41ae522aaa40e6722b99c5b0689ece33c0910
Miklex85 <alexiglesias33@hotmail.com>
2018-03-14 16:32:56 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/example/Configuration.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class Configuration {
+
+    public Map<String, String> getProperties() {
+        Map<String, String> map = new HashMap<>();
+        try {
+            Properties p = new Properties();
+            String file = "/etc/config/archivo.properties";
+            p.load(new FileReader(file));
+            for (String key : p.stringPropertyNames()) {
+                String value = p.getProperty(key);
+                map.put(key, value);
+            }
+        } catch (Exception e) {
+            // Return default values
+            map.put("nombre","OpenShift");
+            map.put("saludo","¡hola!");
+            map.put("mensaje","¡Esto es HelloWorld!");
+        }
+        return map;
+    }
+}
---
+++ src/main/java/com/example/HelloWorldExample.java
@@ -0,0 +1,32 @@
+package com.example;
+ 
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+ 
+public class HelloWorldExample extends HttpServlet {
+
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+               throws IOException, ServletException {
+      response.setContentType("text/html;charset=UTF-8");
+      PrintWriter out = response.getWriter();
+      Configuration configuration = new Configuration();
+      Map properties = configuration.getProperties();
+
+      try {
+         out.println("<!DOCTYPE html>");
+         out.println("<html><head>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+         out.println("<title> OpenShit Hello World App</title></head>");
+         out.println("<body>");
+         out.println("<h1>" + properties.get("saludo") + " " + properties.get("nombre") + "</h1>");
+
+         out.println("<op>" + properties.get("mensaje") + "</p>");
+         out.println("</body></html>");
+      } finally {
+         out.close();
+      }
+   }
+}
